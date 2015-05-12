847b7600a7724c78646313c02c7869abc7d51c05
CWiseguy <CWise-Person@kippnyccp.org>
2015-05-12 13:40:09 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,6 +1,3 @@
-candiate_1=()
-candiate_2=()
-wins=(candiate_1,candiate_2)
 from random import random
 total_wins=0
 for i in range(10000):
@@ -9,11 +6,15 @@ for i in range(10000):
   region1=.87
   region2=.65
   region3=.17
-  if num<=region1:
+  if random() <=region1:
     region_wins=region_wins+1
 #You checking if the region is egual or more to wins then add 1 to get the total of that regent.
-  if num<=region2:
+  if random()<=region2:
     region_wins=region_wins+1
-     if num<=region3: 
+  if random()<=region3: 
     region_wins=region_wins+1
-  percentage=total_wins/10000#Now we looking for the percentage of wins from the total of regions.
+  if region_wins >= 2;
+    total_wins +=1
+    
+percentage=total_wins/float(10000)#Now we looking for the percentage of wins from the total of regions.
+print ("The chances of Winning are (percentage)")
