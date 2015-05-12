113f1181660815958044ef0dca5bf7a62bdb5ee4
CWiseguy <CWise-Person@kippnyccp.org>
2015-05-12 13:15:12 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,5 +1,9 @@
 from random import radiant 
 total=0
-for i in range(1000)
+for i in range(10000)
 if radiant(1,100) <= 87;
-report win
+  wins=wins+1
+if raidant (1,100) <= 65;
+  wins=wins+1
+if radiant(1,100) <= 17
+wins=wins=1
